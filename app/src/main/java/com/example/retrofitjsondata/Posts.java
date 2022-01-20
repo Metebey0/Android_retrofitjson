1c8ad1bbf5a26a2f7043f2bf7c0f525a8599c165
Metebey0 <beyaz-mehmet-t@outlook.com>
2022-01-20 12:15:55 +0300

    Retrofit_json

---
+++ app/src/main/java/com/example/retrofitjsondata/Posts.java
@@ -0,0 +1,28 @@
+package com.example.retrofitjsondata;
+
+public class Posts {
+
+    private String title;
+    private String body;
+
+    public Posts(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
